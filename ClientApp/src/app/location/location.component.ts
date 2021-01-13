c348b076702e711d84be09aaaa371c88a9607b99
Valravn87 <ca11887@hotmail.com>
2021-01-13 16:34:23 -0500

    Update location.component.ts

--- ClientApp/src/app/location/location.component.ts
+++ ClientApp/src/app/location/location.component.ts
@@ -2,8 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-location',
-  templateUrl: './location.component.html',
-  styleUrls: ['./location.component.css']
+  templateUrl: './location.component.html'
 })
 export class LocationComponent implements OnInit {
 
