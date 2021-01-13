4d9bd53e0b9ce8c725f2cf59bae08593b870ca57
Valravn87 <ca11887@hotmail.com>
2021-01-13 16:35:02 -0500

    Update history.component.ts

--- ClientApp/src/app/history/history.component.ts
+++ ClientApp/src/app/history/history.component.ts
@@ -2,8 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-history',
-  templateUrl: './history.component.html',
-  styleUrls: ['./history.component.css']
+  templateUrl: './history.component.html'
 })
 export class HistoryComponent implements OnInit {
 
